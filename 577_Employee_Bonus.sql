497a09551ed025906bf80c28303264bef688f83b
RakeshKhetavat <rakeshkhetavat@gmail.com>
2026-09-11 21:13:29 +0530

    Create 577_Employee_Bonus.sql

---
+++ 577_Employee_Bonus.sql
@@ -0,0 +1,9 @@
+-- LeetCode #577 - Employee Bonus
+-- Difficulty: Easy
+
+SELECT e.name, b.bonus
+FROM Employee e
+LEFT JOIN Bonus b
+ON e.empId = b.empId
+WHERE b.bonus < 1000
+   OR b.bonus IS NULL;
